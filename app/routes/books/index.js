6423d27c75f76ccde49faf3a260f1b9b094456ef
rnkumar <raja.kumar@certesnetworks.com>
2016-02-02 15:22:41 +0530

    sort works.. with special findAll in front

--- app/routes/books/index.js
+++ app/routes/books/index.js
@@ -2,28 +2,29 @@ import Ember from 'ember';
 
 function filter() {
   return this.store.filter('book', {}, function (aBook) {
-    return aBook.get('isbn') === "ISBN1";
+    return aBook.get('isbn') === "ISBN2";
   });
 }
 
 function sort() {
-  return filter.call(this).then(sortByTitle);
+  var _this = this;
+  var storeRef = this.store;
+  return storeRef.findAll('book').then(function () {
+    return filter.call(_this).then(function (books) {
+      return books.sortBy('title');
+    });
+  });
 }
-
-function sortByTitle(items) { //add the sort method here..
-  return items;
-}
-
 export default Ember.Route.extend({
+
   model: function () {
-    var _this = this;
     return Ember.RSVP.hash({
       books: this.store.findAll('book'),
       filteredBooks: this.store.filter('book', {}, function (aBook) {
         return aBook.get('isbn') === "ISBN2";
       }),
 
-      filteredBooksSorted: sort.call(_this)
+      filteredBooksSorted: sort.call(this)
     });
   }
 });
